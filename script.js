e0b9798858f7aaa6fdd1baaa3bd9b5fe69d89236
Adnene_wsl <adnen.guessoum@gmail.com>
2022-10-10 14:41:48 +0200

    Outline first two functions to build

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // javascript code Here for RPS game
+
+
+function getComputerChoice() {
+	// randomly returns rps
+}
+
+function playsOneRound(playerSelection, computerSelection) {
+	// return result of single round in form of a string
+	// case incencitive input for the player!
+}
+
+
